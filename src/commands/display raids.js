b8e8ce1f7e82621becacecb03179c5478f1b85b9
sumaris <alfred.radu@gmail.com>
2021-01-21 20:00:58 +0100

    hotfix display raids 2

--- src/commands/display raids.js
+++ src/commands/display raids.js
@@ -1,43 +1,52 @@
 const utils = require('../utils.js');
-const { forEach } = require('lodash');
 
 module.exports = {
     name: 'display',
     description: 'List all active raid listings for this channel.',
     aliases: ['disp', 'raiddisp', 'd'],
-    usage: '<optional arg1 user tag (user#1234)>',
+    usage: '<optional arg1 raid>',
     cooldown: 1,
     execute(message, args) {
         const data = [];
         const files = utils.getAllFiles();
 
-        if (!args.length) {
-            data.push('Here\'s a list of all the active raid listings:');
-        } else {
-            let user = message.channel.guild.members.fetch({ cache: false }).then(members => members.find(member => member.user.tag === message.author.tag))
-            data.push('Here\'s a list of all the active raid listings from <@' + user + '> :');
-        }
-
-        const foreach = async () => {
+        const foreach = async (raid) => {
+            let c = 0
             for (const fileName of files) {
                 let fileIds = fileName.split('-');
                 let ch = await message.channel.guild.channels.cache.find(x => x.id == fileIds[0]);
                 if (ch.guild.id == message.channel.guild.id) {
                     let msg = await ch.messages.fetch(fileIds[1], true);
                     let file = utils.readFile(msg);
-                    let pos = '';
-                    if (file.members.length < 5) {
-                        pos = `${file.members.length}/6 members`;
-                    } else {
-                        pos = `Raid team full`;
+                    if (file.raid == raid || !raid) {
+                        let pos = '';
+                        if (file.members.length < 5) {
+                            pos = `${file.members.length}/6 members`;
+                        } else {
+                            pos = `Raid team full`;
+                        }
+                        let msgRow = `${utils.raids[file.raid][0]} -- Leader: <@${file.leader}> -- ${pos}`;
+                        data.push(msgRow);
+                        c+=1;
                     }
-                    let msgRow = `${utils.raids[file.raid][0]} -- Leader: <@${file.leader}> -- ${pos}`;
-                    data.push(msgRow);
                 }
             }
-            data.push('\n See the pinned messages for active raid listings.');
-            message.channel.send(data, { split: true });
+            
+            if (c < 1){
+                message.channel.send('Could not find any listings for that raid. Why not create one? \n *!raid <arg1 raid name> <arg2 time dd hh:mm> <arg3 additional info>*');
+            } else {
+                data.push('\n See the pinned messages for active raid listings.');
+                message.channel.send(data, { split: true });
+            }
         }
-        foreach();
+
+        if (!args.length) {
+            data.push('Here\'s a list of all the active raid listings:');
+            foreach();
+        } else {
+            let r = utils.raids[args[0]] ? utils.raids[args[0]][0] : args[0];
+            data.push('Here\'s a list of all the active raids for: ' + r + ' :');
+            foreach(args[0]);
+        }
     }
 };
